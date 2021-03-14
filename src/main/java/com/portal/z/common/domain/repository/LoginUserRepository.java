b4a218b47fdf341176193b6fece87c6f1444be7d
鈴木尚 <suzuki196906@gmail.com>
2021-03-14 18:32:39 +0900

    Issue #107 対応 SQLの様式修正

--- src/main/java/com/portal/z/common/domain/repository/LoginUserRepository.java
+++ src/main/java/com/portal/z/common/domain/repository/LoginUserRepository.java
@@ -35,14 +35,31 @@ public class LoginUserRepository {
     private MassageUtils massageUtils;
 
     /** ユーザー情報を取得するSQL */
-    private static final String SELECT_USER_SQL = " select" + "    user_id" + "   ,user_due_date" + "   ,password"
-            + "   ,pass_update" + "   ,login_miss_times" + "   ,lock_flg" + "   ,enabled_flg" + " from"
-            + "   zm001_user" + " where" + "   user_id = ?";
+    private static final String SELECT_USER_SQL 
+           = " select" 
+        + "    user_id"
+        + "   ,user_due_date"
+        + "   ,password"
+        + "   ,pass_update"
+        + "   ,login_miss_times"
+        + "   ,lock_flg"
+        + "   ,enabled_flg"
+        + " from"
+        + "   zm001_user"
+        + " where"
+        + "   user_id = ?";
 
     /** 権限リストを取得するSQL */
-    private static final String SELECT_USER_ROLE_SQL = " select" + "   coalesce(C.role_name,'NOTHING') as role_name"
-            + " from" + "   zm001_user as A" + " left join zm003_userrole as B" + "   on A.user_id = B.user_id"
-            + " left join zm002_role as C" + "   on B.role_id = C.role_id" + " where" + "   A.user_id = ?";
+    private static final String SELECT_USER_ROLE_SQL
+        = " select" + "   coalesce(C.role_name,'NOTHING') as role_name"
+        + " from"
+        + "   zm001_user as A"
+        + " left join zm003_userrole as B"
+        + "   on A.user_id = B.user_id"
+        + " left join zm002_role as C"
+        + "   on B.role_id = C.role_id"
+        + " where"
+        + "   A.user_id = ?";
 
     /**
      * ユーザー情報を取得して、UserDetailsを生成するメソッド.
