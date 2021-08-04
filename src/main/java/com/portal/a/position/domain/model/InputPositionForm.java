7a5670f44fa12360c06de32a26f1a100fba834bb
izumiryuuki <salvare120301_pann@outlook.jp>
2021-08-04 22:32:06 +0900

    issue #271_test

--- src/main/java/com/portal/a/position/domain/model/InputPositionForm.java
+++ src/main/java/com/portal/a/position/domain/model/InputPositionForm.java
@@ -13,25 +13,25 @@ import lombok.Data;
 public class InputPositionForm {
     // 必須入力
     @NotBlank(message = "{require_check}")
-    @Size(min = 5, max = 5, message = "{length_check_2}") // 桁数指定
+    @Size(min = 5, max = 5,message = "{length_check_1}") // 桁数指定
     private String position_cd; // 役職CD
     
     // 必須入力
     @NotBlank(message = "{require_check}")
-    @Size(min = 1, max = 50, message = "{length_check}") // 桁数指定
+    @Size(max = 50, message = "{length_check_2}") // 桁数指定
     private String position_name; // 役職名
     
     // 必須入力
     @NotBlank(message = "{require_check}")
-    @Size(min = 6, max = 6, message = "{length_check_2}") // 桁数指定
+    @Size(min = 6, max = 6, message = "{length_check_1}") // 桁数指定
     @Pattern(regexp = "[0-9]*", message = "{type_check_2}") // 数字であること
     private String start_yearmonth; // 開始年月
     
 
-    @Size(min = 6, max = 6, message = "{length_check_2}") // 桁数指定
+    @Size(min = 6, max = 6, message = "{length_check_1}") // 桁数指定
     @Pattern(regexp = "[0-9]*", message = "{type_check_2}") // 数字であること
     private String end_yearmonth; // 最終年月
 
-    @Size(max = 100, message = "{length_check_3}")
+    @Size(max = 100, message = "{length_check_2}")
     private String biko; // 備考
 }
