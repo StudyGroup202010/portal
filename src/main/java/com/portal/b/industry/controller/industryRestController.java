0bb54f75311464a6c66335fa0e0e0cc72ade14d1
鈴木尚 <suzuki196906@gmail.com>
2022-03-31 14:29:11 +0900

    * [update] #166対応　REST対応
    ・CORS設定を実装

--- src/main/java/com/portal/b/industry/controller/industryRestController.java
+++ src/main/java/com/portal/b/industry/controller/industryRestController.java
@@ -3,9 +3,7 @@ package com.portal.b.industry.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.portal.b.common.domain.model.Industry;
@@ -24,14 +22,11 @@ public class industryRestController {
     /**
      * 産業分類マスタのGET用メソッド.
      * 
-     * @param model Modelクラス
-     * @return 画面のテンプレート名
+     * @return industryService.selectMany()
      */
- //   @CrossOrigin
     @GetMapping("/api/industryList")
-    @CrossOrigin(origins = "http://localhost:4200")
-    public List<Industry> getIndustryList(@RequestParam(value = "name", defaultValue = "World") String name) {
-        // 産業分類マスタ一覧の生成
+    public List<Industry> getIndustryList() {
+        // 産業分類マスタ一覧を返信
         return industryService.selectMany();
     }
 }
