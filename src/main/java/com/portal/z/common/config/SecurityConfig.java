2d362f4e49d1a67eee3d4435bd046fd89e07b9b4
鈴木尚 <suzuki196906@gmail.com>
2024-05-26 22:50:50 +0900

    権限設定追加

--- src/main/java/com/portal/z/common/config/SecurityConfig.java
+++ src/main/java/com/portal/z/common/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
